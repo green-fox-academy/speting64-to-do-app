14e1146d734ee032aa682178ce6441c8acbbd13e
Viktor Szabo <viklukacs64@gmail.com>
2017-09-28 14:06:57 +0200

    PrintUsage

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void writeLines(Path myPath) {
+
+        //Path myPath = Paths.get("C://Users/Viktorlukács/TodoApp/src/printusage.txt");
+        myPath = Paths.get("/src/printusage.txt");
+
+        try {
+            ArrayList<String> lines = new ArrayList<>(Files.readAllLines(myPath));
+            //List<String> lines = Files.readAllLines(myPath);
+            for (int i = 0; i < lines.size(); i++) {
+                System.out.println(lines.get(i));
+            }
+            /*for (String line : lines) {
+                System.out.println(line);
+            }*/
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            //System.out.println("Unable to read file: my-file.txt");
+        }
+    }
+}
---
+++ src/MainUsage.java
@@ -0,0 +1,8 @@
+import java.nio.file.Paths;
+
+public class MainUsage {
+        public static void main(String[] args) {
+            Main.writeLines(Paths.get( "/src/printusage.txt"));
+        }
+    }
+
